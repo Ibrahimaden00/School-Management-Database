04e0230ec28215277e1de6d4dcc46ce91af2f526
Oras94 <93916020+Oras94@users.noreply.github.com>
2022-02-05 18:48:07 +0100

    Add files via upload
    Lagt till 10 studenter

---
+++ SQLQuery1 10 Students.sql
@@ -0,0 +1,12 @@
+INSERT INTO [Student] (StudentID,FirstName,LastName,Class,Age,PhoneNumber,Adress)
+VALUES
+  (10001,'Mike','Cairo','8B',14,'(656) 708-8366','Ap #682-9446 Massa Rd.'),
+  (10002,'Brenna','Danielle','7A',13,'(220) 642-4667','580-8492 Felis St.'),
+  (10003,'Kylynn','Hayley','7B',13,'(974) 632-5108','602-8381 Class Rd.'),
+  (10004,'Aladdin','Tanner','8C',14,'(489) 211-2387','456-4573 Gravida Av.'),
+  (10005,'Aphrodite','Lydia','9B',15,'(521) 652-9413','P.O. Box 113, 6186 Vitae Avenue'),
+  (10006,'Quin','Hyacinth','7C',13,'(879) 394-3601','6858 Donec Ave'),
+  (10007,'Alexander','Kay','7C',13,'(305) 828-7427','479-5836 Imperdiet Road'),
+  (10008,'Aurelia','Sage','8A',14,'(943) 793-7224','711-655 Sem Rd.'),
+  (10009,'Daphne','Rhea','7C',13,'(813) 248-8877','216-9938 Praesent Street'),
+  (10010,'Jasper','Noble','7B',13,'(668) 351-2375','6073 Suspendisse St.');
