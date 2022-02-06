f98dcf62de96653f645dea735fb44bd2dcb33464
Oras94 <93916020+Oras94@users.noreply.github.com>
2022-02-06 18:52:50 +0100

    Add files via upload
    Mer studenter

---
+++ SQLQuery3 Students.sql
@@ -0,0 +1,23 @@
+INSERT INTO [Student](StudentID,FirstName,LastName,Class,Age,PhoneNumber,Adress)
+Values
+
+  (10041,'Driscoll','Hunter','8B',14,'(254) 719-2176','583-1795 Aliquet St.'),
+  (10042,'Virginia','Amal','9A',15,'(825) 676-5215','Ap #400-9615 Arcu St.'),
+  (10043,'Lacey','Ina','9A',15,'(712) 746-7323','468-1975 Massa. Street'),
+  (10044,'Kibo','Timothy','7B',13,'(882) 457-6922','Ap #142-8962 Nonummy St.'),
+  (10045,'Ishmael','Dieter','7C',13,'(661) 117-2407','Ap #738-6318 Tincidunt, Street'),
+  (10046,'Lesley','September','9A',15,'(738) 915-2836','591-1015 Ac St.'),
+  (10047,'Linus','Holmes','7B',13,'(681) 487-7557','475-4072 Luctus Road'),
+  (10048,'Madison','Jaden','8B',14,'(577) 350-3540','Ap #808-1725 Faucibus Road'),
+  (10049,'Todd','Hayfa','7C',13,'(433) 249-7956','P.O. Box 311, 5443 Tristique St.'),
+  (10050,'Kasimir','Lyle','7A',13,'(845) 814-5454','169-104 Nullam Avenue'),
+  (10051,'Charde','Ian','8C',14,'(536) 436-2631','Ap #587-1738 Consequat Street'),
+  (10052,'Wesley','Summer','8A',14,'(287) 983-2320','803-2164 Duis Av.'),
+  (10053,'Miriam','Jacob','9A',15,'(427) 458-4628','Ap #588-8971 Magna Avenue'),
+  (10054,'Aladdin','Gage','8B',14,'(185) 637-9635','2799 Etiam Street'),
+  (10055,'Lunea','Carlos','8B',14,'(464) 345-6524','Ap #927-2551 Tristique Rd.'),
+  (10056,'Avram','Carter','7B',13,'(865) 858-4871','971-8085 Nascetur Rd.'),
+  (10057,'Tara','Tallulah','8C',14,'(291) 565-9393','985-8294 Lacus Road'),
+  (10058,'Piper','Lareina','7B',13,'(541) 241-3843','476-8067 Et Av.'),
+  (10059,'Fallon','Akeem','7A',13,'(420) 195-5348','P.O. Box 875, 7687 Eu St.'),
+  (10060,'Tad','Mohammad','7A',13,'(471) 881-7075','619-7745 Purus. St.');
