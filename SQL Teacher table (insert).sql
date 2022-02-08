ddb3192948038971920e1dfed876528bbbdc09d1
SurfnShark <98813043+SurfnShark@users.noreply.github.com>
2022-02-08 10:52:15 +0100

    Add files via upload
    Teacher insert into

---
+++ SQL Teacher table (insert).sql
@@ -0,0 +1,9 @@
+insert into [Teacher] (TeacherID, TeacherFirsName, TeacherLastName, Expertise)
+values(0001, 'Xavier','Knox','Spanish'),
+      (0002, 'Lois','Alexander','Math'),
+      (0003, 'Danielle','Amir','Science'),
+      (0004, 'Finn','Brendan','Social Science'),
+      (0006, 'John','Scott','Physical Education'),
+	  (0007, 'Martin','Karlson','Swedish'),
+	  (0008, 'Dick','Bert','Art'),
+	  (0009, 'Frank','Sinatra','Music');
